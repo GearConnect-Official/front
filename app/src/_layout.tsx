1ef5eb00521800632101e0e62fd9717cb6e0ac17
Romjah <romjah@orange.fr>
2025-11-05 15:13:30 +0100

    Remove unused import from layout file to streamline code

--- app/src/_layout.tsx
+++ app/src/_layout.tsx
@@ -1,6 +1,5 @@
 // This layout file prevents Expo Router from treating files in this directory as routes
 // All files in app/src/ are utility files (components, services, styles, etc.) and not routes
-import { Stack } from 'expo-router';
 
 export default function SrcLayout() {
   // This directory contains only utility files, not routes
